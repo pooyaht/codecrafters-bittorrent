a4d23b7a8ef6e1636292125c5718af32871576a8
pooyaht <pooyahatami83@gmail.com>
2024-10-05 15:32:10 +0330

    Implement info command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io::Read};
 
 mod decoder;
 mod error;
@@ -18,6 +18,31 @@ fn main() {
             panic!("{}", decoded_value.err().unwrap());
         }
         println!("{}", decoded_value.unwrap());
+    } else if command == "info" {
+        let mut buffer = Vec::new();
+        std::fs::File::open(args[2].as_str())
+            .unwrap()
+            .read_to_end(&mut buffer)
+            .unwrap();
+
+        let mut bencode_decoder = decoder::BenCodeDecoder::new(&buffer);
+        let decoded_value = bencode_decoder.decode();
+        if decoded_value.is_err() {
+            panic!("{}", decoded_value.err().unwrap());
+        }
+        let decoded_value = decoded_value.unwrap();
+
+        if let Some(announce) = decoded_value["announce"].as_str() {
+            println!("Tracker URL: {}", announce);
+        } else {
+            println!("Tracker URL not found or not a string");
+        }
+
+        if let Some(length) = decoded_value["info"]["length"].as_i64() {
+            println!("Length: {} bytes", length);
+        } else {
+            println!("Length not found or not an integer");
+        }
     } else {
         println!("unknown command: {}", args[1])
     }
