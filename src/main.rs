2ebd4f50b840c11078a93d5ecadb3c79ef8364e0
pooyaht <pooyahatami83@gmail.com>
2024-10-05 08:59:41 +0330

    Add basic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
+use anyhow::Result;
+
+#[derive(Debug)]
+enum Error {
+    BencodeStringParseError,
+    NotNumberError(String),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::BencodeStringParseError => write!(f, "bencode string parse error"),
+            Error::NotNumberError(number) => write!(f, "not a number: {}", number),
+        }
+    }
+}
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, Error> {
     // If encoded_value starts with a digit, it's a number
     if encoded_value.chars().next().unwrap().is_ascii_digit() {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
+        let colon_index = encoded_value
+            .find(':')
+            .ok_or(Error::BencodeStringParseError)?;
         let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
+        let number = number_string
+            .parse::<i64>()
+            .map_err(|_| Error::NotNumberError(number_string.to_string()))?;
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        serde_json::Value::String(string.to_string())
+        Ok(serde_json::Value::String(string.to_string()))
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -26,7 +44,11 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value);
+        if decoded_value.is_err() {
+            println!("{}", decoded_value.err().unwrap());
+            return;
+        }
+        println!("{}", decoded_value.unwrap());
     } else {
         println!("unknown command: {}", args[1])
     }
