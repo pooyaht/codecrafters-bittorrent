e8eab781fe9d635f4f4a195c5f1295880590fd60
pooyaht <pooyahatami83@gmail.com>
2024-10-05 18:02:29 +0330

    Implement Info Hash calculation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::{env, io::Read};
 
 mod decoder;
+mod encoder;
 mod error;
 
+use encoder::encode;
+
 pub(crate) use error::*;
+use sha1::Digest;
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
@@ -30,20 +34,30 @@ fn main() {
         if decoded_value.is_err() {
             panic!("{}", decoded_value.err().unwrap());
         }
+
         let decoded_value = decoded_value.unwrap();
-
-        if let Some(announce) = decoded_value["announce"].as_str() {
-            println!("Tracker URL: {}", announce);
-        } else {
-            println!("Tracker URL not found or not a string");
-        }
-
-        if let Some(length) = decoded_value["info"]["length"].as_i64() {
-            println!("Length: {} bytes", length);
-        } else {
-            println!("Length not found or not an integer");
-        }
+        info_command(decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
+}
+
+fn info_command(decoded_value: serde_json::Value) {
+    if let Some(announce) = decoded_value["announce"].as_str() {
+        println!("Tracker URL: {}", announce);
+    } else {
+        println!("Tracker URL not found or not a string");
+    }
+
+    if let Some(length) = decoded_value["info"]["length"].as_i64() {
+        println!("Length: {}", length);
+    } else {
+        println!("Length not found or not an integer");
+    }
+
+    let bencoded_info = encode(&decoded_value["info"]).unwrap_or_default();
+    println!(
+        "Info Hash: {}",
+        format_args!("{:x}", sha1::Sha1::digest(bencoded_info))
+    );
 }
