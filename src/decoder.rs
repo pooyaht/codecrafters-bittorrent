fc28ff244c56fa659ad39e74cc23f265b33d091e
pooyaht <pooyahatami83@gmail.com>
2024-10-05 09:16:35 +0330

    Move bencode decoder logic into its own file

---
+++ src/decoder.rs
@@ -0,0 +1,33 @@
+use crate::Error;
+
+pub(crate) struct BenCodeDecoder<'a> {
+    pub(crate) input: &'a str,
+}
+
+impl<'a> BenCodeDecoder<'a> {
+    pub(crate) fn new(input: &'a str) -> Self {
+        Self { input }
+    }
+
+    pub(crate) fn decode(&self) -> Result<serde_json::Value, Error> {
+        match self.input.chars().next() {
+            Some(digit) if digit.is_ascii_digit() => self.parse_bencode_string(),
+            _ => todo!(),
+        }
+    }
+
+    fn parse_bencode_string(&self) -> Result<serde_json::Value, crate::Error> {
+        let encoded_value = self.input;
+
+        let colon_index = encoded_value
+            .find(':')
+            .ok_or(Error::BencodeStringParseError)?;
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string
+            .parse::<i64>()
+            .map_err(|_| Error::NotNumberError(number_string.to_string()))?;
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+
+        Ok(serde_json::Value::String(string.to_string()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 use std::env;
 
+mod decoder;
 mod error;
 
-use anyhow::Result;
 pub(crate) use error::*;
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, Error> {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value
-            .find(':')
-            .ok_or(Error::BencodeStringParseError)?;
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string
-            .parse::<i64>()
-            .map_err(|_| Error::NotNumberError(number_string.to_string()))?;
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        Ok(serde_json::Value::String(string.to_string()))
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-}
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
@@ -30,11 +11,11 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let bencode_decoder = decoder::BenCodeDecoder::new(&args[2]);
+
+        let decoded_value = bencode_decoder.decode();
         if decoded_value.is_err() {
-            println!("{}", decoded_value.err().unwrap());
-            return;
+            panic!("{}", decoded_value.err().unwrap());
         }
         println!("{}", decoded_value.unwrap());
     } else {
