5580bec706c42c8fe5085d1fdd01de336e1a0c8c
pooyaht <pooyahatami83@gmail.com>
2024-10-05 09:17:05 +0330

    Implement bencode integer decoding

--- src/decoder.rs
+++ src/decoder.rs
@@ -12,6 +12,7 @@ impl<'a> BenCodeDecoder<'a> {
     pub(crate) fn decode(&self) -> Result<serde_json::Value, Error> {
         match self.input.chars().next() {
             Some(digit) if digit.is_ascii_digit() => self.parse_bencode_string(),
+            Some('i') if self.input.ends_with('e') => self.parse_bencode_integer(),
             _ => todo!(),
         }
     }
@@ -29,5 +30,16 @@ impl<'a> BenCodeDecoder<'a> {
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
 
         Ok(serde_json::Value::String(string.to_string()))
+    }
+
+    fn parse_bencode_integer(&self) -> Result<serde_json::Value, Error> {
+        let encoded_value = self.input;
+
+        let number_string = &encoded_value[1..encoded_value.len() - 1];
+        let number = number_string
+            .parse::<i64>()
+            .map_err(|_| Error::NotNumberError(number_string.to_string()))?;
+
+        Ok(serde_json::Value::Number(number.into()))
     }
 }
